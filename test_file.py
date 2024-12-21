242dde637db23db1f7633a24ced59b11461222a1
r-log <rkkundid9@gmail.com>
2024-12-21 21:43:32 +0100

    test: Added docstrings, return type hints, changed z to Z since is treater as a constant

--- test_file.py
+++ test_file.py
@@ -1,10 +1,18 @@
 def bad_function() -> None:
+    """Example function to demonstrate code review functionality.
+
+    Returns:
+        None: This function returns None
+    """
     X_VALUE = 1
     Y_VALUE = 2
-    z = X_VALUE + Y_VALUE
-    return z
+    Z = X_VALUE + Y_VALUE  # Changed to uppercase for constant
+    return Z
 
 
-def unused_function():
-    """This is an unused function."""
+def unused_function() -> None:
+    """Example of an unused function.
+
+    This function exists to test the unused code detection.
+    """
     pass
