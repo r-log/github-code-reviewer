184ef0f566b55651f1e8b81618d964f7446f445b
r-log <rkkundid9@gmail.com>
2024-12-21 20:04:06 +0100

    test: Added test file to test the github action

---
+++ test_file.py
@@ -0,0 +1,9 @@
+def badFunction():
+    x = 1
+    y = 2
+    z = x+y  # No spaces around operators
+    return z  # Missing type hints
+
+
+def unused_function():  # This will be flagged as unused
+    pass
